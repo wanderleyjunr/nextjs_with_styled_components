7e2b0acb70dc90efd6a1703b51dc13b734f6d015
WanderleyJunior <wanderleyjunr@gmail.com>
2021-08-17 07:43:59 -0300

    refactoring storybook settings and adding assets folder in public

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,29 @@
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'light',
+    values: [
+      {
+        name: 'light',
+        value: theme.colors.white,
+      },
+      {
+        name: 'dark',
+        value: theme.colors.primaryColor,
+      },
+    ],
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <Story />
+      <GlobalStyles />
+    </ThemeProvider>
+  ),
+];
